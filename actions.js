13eb3ef0a9804be96737731a9ee74e35b96287a4
OlhaMazurenko <mail.olgamazurenko@gmail.com>
2019-06-24 18:00:24 +0300

    'Solution'

--- actions.js
+++ actions.js
@@ -1,28 +1,28 @@
-export const SELECT_ITEM = 'SELECT_ITEM';
-export const CLEAR_SELECTION = 'CLEAR_SELECTION';
-export const MOVE_SELECTION_UP = 'MOVE_SELECTION_UP';
-export const MOVE_SELECTION_DOWN = 'MOVE_SELECTION_DOWN';
+ const SELECT_ITEM = 'SELECT_ITEM';
+ const CLEAR_SELECTION = 'CLEAR_SELECTION';
+ const MOVE_SELECTION_UP = 'MOVE_SELECTION_UP';
+ const MOVE_SELECTION_DOWN = 'MOVE_SELECTION_DOWN';
 
-export const selectItem = (index) => {
+ const selectItem = (index) => {
   return {
     type: SELECT_ITEM,
     payload: index,
   }
 };
 
-export const clearSelection = () => {
+ const clearSelection = () => {
   return {
     type: CLEAR_SELECTION,
   }
 };
 
-export const moveSelectionUp = () => {
+ const moveSelectionUp = () => {
   return {
     type: MOVE_SELECTION_UP,
   }
 };
 
-export const moveSelectionDown = () => {
+ const moveSelectionDown = () => {
   return {
     type: MOVE_SELECTION_DOWN,
   }
