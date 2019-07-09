eb0e19f3835aab71dc3c151da4cd810c6d69a592
Ivan Deiko <ivandeiko1997@gmail.com>
2019-07-09 09:29:47 +0300

    'fix'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,63 @@
 'use strict'
+
+const MOVE_UP = 'move_up';
+const MOVE_DOWN = 'move_down';
+const SELECT_INDEX = 'selected_index'
+
+function moveUpAction() {
+  return {
+    type: MOVE_UP
+  };
+};
+
+function moveDownAction() {
+  return {
+    type: MOVE_DOWN
+  };
+};
+
+function selectedIndexAction(index) {
+  return {
+    type: SELECT_INDEX,
+    index
+  };
+};
+
+let initialState = {
+  foods: ['Apple', 'Bread', 'Carrot', 'Dumplings', 'Eggs', 'Fish', 'Garlic', 'Honey', 'Ice cream', 'Jam'],
+  selectedFoodIndex: null
+};
+
+function getNextState(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_UP: 
+      const newFoodItemsUp = [...state.foods];
+      const indexFoodItemUp = state.selectedFoodIndex;
+      const [selectedFoodItemUp] = newFoodItemsUp.splice(indexFoodItemUp, 1);
+      newFoodItemsUp.splice(indexFoodItemUp - 1, 0, selectedFoodItemUp);
+      return {
+        foods: newFoodItemsUp,
+        selectedFoodIndex: indexFoodItemUp - 1
+      };
+    case MOVE_DOWN:
+      const newFoodItemsDown = [...state.foods];
+      const indexFoodItemDown = state.selectedFoodIndex;
+      const [selectedFoodItemDown] = newFoodItemsDown.splice(indexFoodItemDown, 1);
+      newFoodItemsDown.splice(indexFoodItemDown + 1, 0, selectedFoodItemDown);
+      return {
+        foods: newFoodItemsDown,
+        selectedFoodIndex: indexFoodItemDown + 1
+      };
+    case SELECT_INDEX:
+      return {
+        ...state,
+        selectedFoodIndex: action.index
+      };
+    default:
+      return state;
+  };
+};
+
 let store = Redux.createStore(getNextState);
 
 const moveDowm = document.querySelector('#moveDown');
@@ -13,6 +72,7 @@ moveUp.addEventListener('click', () => {
 });
 
 function render() {
+  console.log('ssss')
   const listItem = document.querySelector('#listItem');
   listItem.innerHTML = '';
 
