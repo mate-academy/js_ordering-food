07981e97380b17c93a43abd47c1180e35c94d80f
tyooma <flying.alien.godzilla@gmail.com>
2020-07-20 13:58:58 +0300

    Added actions & actionsCreator objects

--- store.js
+++ store.js
@@ -1,12 +1,16 @@
 const { createStore } = require('./redux.min');
 
-const SELECT = 'select';
-const MOVE_UP = 'move_up';
-const MOVE_DOWN = 'move_down';
+const actions = {
+  SELECT: 'SELECT',
+  MOVE_UP: 'MOVE_UP',
+  MOVE_DOWN: 'MOVE_DOWN',
+};
 
-const select = (index) => ({ type: SELECT, index });
-const up = () => ({ type: MOVE_UP });
-const down = () => ({ type: MOVE_DOWN });
+const actionsCreator = {
+  select: (index) => ({type: actions.SELECT, index}),
+  moveUp: () => ({type: actions.MOVE_UP}),
+  moveDown: () => ({type: actions.MOVE_DOWN}),
+};
 
 const initialState = {
   items: [
@@ -26,22 +30,30 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case SELECT:
+    case actions.SELECT:
       return {
         ...state,
         index: action.index,
       };
-    case MOVE_UP:
+    case actions.MOVE_UP:
       const itemsToMoveUp = [...state.items];
-      [itemsToMoveUp[state.index], itemsToMoveUp[state.index - 1]] = [itemsToMoveUp[state.index + 1], itemsToMoveUp[state.index]]
+      [
+        itemsToMoveUp[state.index], itemsToMoveUp[state.index - 1]
+      ] = [
+        itemsToMoveUp[state.index + 1], itemsToMoveUp[state.index]
+      ]
       return {
         ...state,
         items: itemsToMoveUp,
         index: state.index - 1,
       };
-    case MOVE_DOWN:
+    case actions.MOVE_DOWN:
       const itemsToMoveDown = [...state.items];
-      [itemsToMoveDown[state.index], itemsToMoveDown[state.index + 1]] = [itemsToMoveDown[state.index + 1], itemsToMoveDown[state.index]]
+      [
+        itemsToMoveDown[state.index], itemsToMoveDown[state.index + 1]
+      ] = [
+        itemsToMoveDown[state.index + 1], itemsToMoveDown[state.index]
+      ]
       return {
         ...state,
         items: itemsToMoveDown,
@@ -54,9 +66,10 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 
-store.dispatch(select(2));
-store.dispatch(down());
-store.dispatch(down());
+console.log(store.getState());
+store.dispatch(actionsCreator.select(5));
+store.dispatch(actionsCreator.moveDown());
+store.dispatch(actionsCreator.moveDown());
 console.log(store.getState());
 
 module.exports = {
