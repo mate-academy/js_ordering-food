6ff45c3b786f5a1db8f8d9e9111562b60178abca
DimaDP <dimadp95@gmail.com>
2020-07-22 21:55:21 +0300

    fix

--- store.js
+++ store.js
@@ -1,34 +1,67 @@
 const { createStore } = require('./redux.min');
 
 let initialtState = {
-  items: ['Apple', 'Bread', 'Carrot', 'Dumplings', 'Eggs', 'Fish', 'Garlic', 'Honey', 'Ice cream', 'Jam'],
+  items: [
+    'Apple',
+    'Bread',
+    'Carrot',
+    'Dumplings',
+    'Eggs',
+    'Fish',
+    'Garlic',
+    'Honey',
+    'Ice cream',
+    'Jam'
+    ],
   selectedIndex: 0,
 }
 
+const actions = {
+  MOVE_UP: "MOVE_UP",
+  MOVE_DOWN: "MOVE_DOWN",
+  SELECT: "SELECT",
+}
 
 const reducer = (state = initialtState, action) => {
 
   switch (action.type) {
 
-    case "MOVE_UP":
-      const changedItems1 = [...state.items];
-      const index1 = state.selectedIndex;
-      [changedItems1[index1], changedItems1[index1 - 1]] = [changedItems1[index1 - 1], changedItems1[index1]];
-
-      return {...state, items: changedItems1, selectedIndex: index1 - 1};
-      
-    case "MOVE_DOWN":
+    case actions.MOVE_UP: {
       const changedItems = [...state.items];
-      const index = state.selectedIndex;
-      [changedItems[index], changedItems[index + 1]] = [changedItems[index + 1], changedItems[index]];
+      const { selectedIndex } = state;
+      [
+        changedItems[selectedIndex], 
+        changedItems[selectedIndex - 1]
+      ] = [
+        changedItems[selectedIndex - 1],
+        changedItems[selectedIndex]
+      ];
+
+      return {
+        ...state,
+        items: changedItems, 
+        selectedIndex: selectedIndex - 1
+      }
+    }
+      
+    case actions.MOVE_DOWN:
+      const changedItems = [...state.items];
+      const { selectedIndex } = state;
+      [
+        changedItems[selectedIndex],
+         changedItems[selectedIndex + 1]
+      ] = [
+          changedItems[selectedIndex + 1],
+          changedItems[selectedIndex]
+      ];
 
       return {
         ...state, 
         items: changedItems,
-        selectedIndex: index + 1
+        selectedIndex: selectedIndex + 1
         };
         
-    case "SELECT":
+    case actions.SELECT:
 
       return {
         ...state,
@@ -40,41 +73,19 @@ const reducer = (state = initialtState, action) => {
   }
 }
 
-const moveUp = () => {
-  return {
-    type: "MOVE_UP"
-  }
-}
-
-const moveDown = () => {
-  return {
-    type: "MOVE_DOWN"
-  }
-}
-
-const selecItem = (index) => {
-  return {
-    type: "SELECT",
-    currentIndex: index,
-  }
-}
+const moveUp = () => ({type: actions.MOVE_UP});
+const moveDown = () => ({type:  actions.MOVE_DOWN});
+const selecItem = (index) => ({type: actions.SELECT, currentIndex: index});
 
 const store = createStore(reducer);
-
-
-const actions = {
-  MOVE_UP: "MOVE_UP",
-  MOVE_DOWN: "MOVE_DOWN",
-  SELECT: "SELECT",
-}
-
-
 const initialState = store.getState();
+
 console.log(initialState.items);
 store.dispatch(selecItem(0));
 store.dispatch(moveDown());
+store.dispatch(moveUp());
 store.dispatch(moveDown());
 
 const updatedState = store.getState();
 console.log(updatedState.items)
-console.log('Should be true', initialState.items[0] === updatedState.items[2]);
+console.log('Should be true', initialState.items[0] === updatedState.items[1]);
